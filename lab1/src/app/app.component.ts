347b60477635aa8b59a8238841680aabe03d2cfa
kuznetsov371 <kuzya07272@gmail.com>
2021-09-14 21:30:00 +0300

    Revert "Hello"
    This reverts commit 5848f15cfeddfe6fd6a7ebae32727434f2055c85.

--- lab1/src/app/app.component.ts
+++ lab1/src/app/app.component.ts
@@ -1,3 +1,10 @@
-let message: string = 'Hello, World!';
-console.log(message);
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lab1';
+}
